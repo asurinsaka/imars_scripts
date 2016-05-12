b48d5a35ace8ca46d05f209ecb24ef2e31af6b40
asurinsaka <asurinsaka@users.noreply.github.com>
2016-05-12 13:37:00 -0400

    Create similar_pass.py

---
+++ similar_pass.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+import sys
+import os
+from optparse import OptionParser
+from datetime import datetime, timedelta
+from hashlib import md5
+
+
+def compare(root, filea, fileb, do_compare):
+    if not do_compare:
+        return True
+    else:
+        return md5(open(root+'/'+filea, 'rb').read()).digest() == \
+               md5(open(root+'/'+fileb, 'rb').read()).digest()
+
+
+if __name__ == "__main__":
+
+
+    print '''This is a python script to find similar pass from
+final products from NASA'''
+
+
+    # Read commandLine options...
+    version = "%prog 0.1"
+    usage = '''usage: %prog [options] /dir'''
+
+    parser = OptionParser(usage=usage, version=version)
+
+    parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
+                        help="Little Miss Chatterbox...")
+    parser.add_option("-f", "--file", action="store", type="string", dest="filename",
+                      help ="Save the output to a file")
+    parser.add_option("-c", "--compare", action="store_true", dest="compare",
+                      help ="Check to see if files are the same")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) == 1:
+        root_dir = args[0]
+    else:
+        parser.error("Wrong number of arguments")
+
+
+    if options.filename:
+        if os.path.exists(options.filename):
+            print "The file "+options.filename+" already exist!"
+            var = raw_input("Overwrite?(y/N)")
+            if var != "y":
+                sys.exit(1)
+        output_file = open(options.filename, 'w')
+
+    verbose = True if not options.verbose else options.verbose
+
+    if not os.path.isdir(root_dir):
+        print "The directory " + root_dir + " does not exists!"
+        sys.exit(1)
+
+
+
+    for root, dirs, files in os.walk(root_dir):
+        if len(files)>1:
+            files = [ x for x in files if "hdf" not in x ]
+            files.sort()
+            for i in range(len(files)-1):
+                filea = files[i].split('.')
+                fileb = files[i+1].split('.')
+                if filea[0] == fileb[0] and filea[3] == fileb[3] and \
+                    filea[4] == fileb[4] and filea[5] == fileb[5]:
+                    timea = datetime(*map(int, [filea[1][:4], filea[1][4:6], filea[1][6:],
+                                     filea[2][:2], filea[2][2:]]))
+                    timeb = datetime(*map(int, [fileb[1][:4], fileb[1][4:6], fileb[1][6:],
+                                     fileb[2][:2], fileb[2][2:]]))
+                    if timeb - timea == timedelta(seconds=300) and compare(root, files[i], files[i+1], options.compare):
+                        if options.filename:
+                            output_file.write(root+'/'+files[i]+'\n')
+                        if verbose:
+                            print root+'/'+files[i]
+
+    if options.filename:
+        output_file.close()
